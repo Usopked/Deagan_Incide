9200fc27fdcc58351e56ee195789546ab69384f6
usopked <usopked16496@gmail.com>
2024-11-02 14:11:53 +0900

    정렬,배치

--- src/pages/PostItem.js
+++ src/pages/PostItem.js
@@ -7,10 +7,10 @@ function PostItem({ post }) {
     <div className="post-item">
       <Link to={`/post/${post.id}`}>
         <h3>{post.title}</h3>
+        {post.imageUrl && <img src={post.imageUrl} alt="Post Thumbnail" className ="post-thumbnail" />} {/* 포스트 썸네일 이미지 표시 */}
         <p>{post.userNickname}</p>
         <p>{new Date(post.timestamp?.seconds * 1000).toLocaleDateString()}</p>
         <p>댓글 {post.commentsCount}개</p>
-        {post.imageUrl && <img src={post.imageUrl} alt="Post Thumbnail" className ="post-thumbnail" />} {/* 포스트 썸네일 이미지 표시 */}
       </Link>
     </div>
   );
